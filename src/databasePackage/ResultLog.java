ac311c2b381a8dbfc76d621fd4ed62cc8f5a3fa1
kronen77 <even__kj@hotmail.com>
2018-03-21 19:13:40 +0100

    Update ResultLog.java

--- src/databasePackage/ResultLog.java
+++ src/databasePackage/ResultLog.java
@@ -11,43 +11,43 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ResultLog {
-
+	
 	List<String> exe = new ArrayList<>();
 	List<String> results = new ArrayList<>();
-
-	public void listOfResults(String startDate, String endDate, String exercise) throws ParseException {
+	
+	
+	public List<String> listOfResults(String startDate, String endDate, String exercise, String user) throws ParseException{
 		try {
-			String sDate = startDate;
-			SimpleDateFormat sdfStart = new SimpleDateFormat("yyyy-MM-dd");
-			java.util.Date uDate = sdfStart.parse(sDate);
-			Date start = new Date(uDate.getTime());
-
-			String eDate = endDate;
-			SimpleDateFormat sdfEnd = new SimpleDateFormat("yyyy-MM-dd");
-			java.util.Date stDate = sdfEnd.parse(eDate);
-			Date end = new Date(stDate.getTime());
-
+			
 			int id = getExerciseID(exercise);
-
+			int uid = getUserID(user);
+			
 			Connection conn = Connect.getConn();
 			PreparedStatement stmt;
 			String query = "Select R.Dato, R.Resultat from Ovelse AS O "
-					+ "inner join ResultatI AS RI ON O.OvelseID = RI.OvelseID"
-					+ "INNER JOIN Resultater AS R ON RI.ResultatID = R.ResultatID" + "WHERE R.Dato between ? and ? ";
-
+					+ "inner join ResultatI AS RI ON (O.OvelseID = RI.OvelseID)"
+					+ " INNER JOIN Resultater AS R ON (RI.ResultatID = R.ResultatID)"
+					+ " WHERE O.OvelseID = ? and R.Dato between ? and ? ";
 			stmt = conn.prepareStatement(query);
+			stmt.setInt(1, uid);
+			stmt.setDate(2, Date.valueOf(startDate));
+			stmt.setDate(3, Date.valueOf(endDate));
 			ResultSet rs = stmt.executeQuery();
-			while (rs.next()) {
-				results.add(rs.getString("Dato") + rs.getString("Resultater"));
+			while(rs.next()) {
+					results.add(rs.getString("Dato") + " " + rs.getString("Resultat"));
 			}
-			System.out.println();
+			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
+		return results;
+		
 	}
-
+	
+	
+	
+	
 	public void createExerciseList() {
 		Connection conn = Connect.getConn();
 		PreparedStatement stmt;
@@ -63,8 +63,7 @@ public class ResultLog {
 			e.printStackTrace();
 		}
 	}
-
-	public int getExerciseID(String �velsenavn) {
+	public int getExerciseID(String øvelsenavn) {
 		
 		//initilize uid
 		int eid= 0;
@@ -72,7 +71,7 @@ public class ResultLog {
 		try {
 			Connection conn = Connect.getConn();
 			PreparedStatement stmt = conn.prepareStatement("Select OvelseID from Ovelse where Navn=?"); 
-			stmt.setString(1, �velsenavn);
+			stmt.setString(1, øvelsenavn);
 			ResultSet rs = stmt.executeQuery();
 			while(rs.next()) {
 				eid = (rs.getInt(1)); 
@@ -84,4 +83,24 @@ public class ResultLog {
 			return -1;
 		}
 	}
+	public int getUserID(String username) {
+			
+			//initilize uid
+			int uid= 0;
+			
+			try {
+				Connection conn = Connect.getConn();
+				PreparedStatement stmt = conn.prepareStatement("Select BrukerID from Bruker where Navn=?"); 
+				stmt.setString(1, username);
+				ResultSet rs = stmt.executeQuery();
+				while(rs.next()) {
+					uid = (rs.getInt(1)); 
+				}
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				return -1;
+			}
+			return uid;
+		}
 }
